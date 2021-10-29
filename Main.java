b117f5d983ffefdf6823fb4fba267f581ecb528c
elizaveta199 <93157218+elizaveta199@users.noreply.github.com>
2021-10-29 16:01:49 +0300

    homework3

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean x=Utils.winOrLose(5, false);
+        if (x==true) {System.out.println("Игрок выйграл");} else {System.out.println("Игрок проиграл");};
+    }
+}
---
+++ Utils.java
@@ -0,0 +1,11 @@
+public class Utils {
+
+    public static boolean winOrLose ( int speedOfPlayer, boolean isGreenLight) {
+        // boolean isGreenLight=false;
+        boolean x=false;
+        if ((!isGreenLight)&&(speedOfPlayer>0)) {x=false;} else {x=true;};
+        return  x;
+    }
+
+}
+
